b501df6dea62e11c085117e22cd56a3c4b2fe9ce
Junlapong L <junlapong@gmail.com>
2021-06-19 23:25:48 +0700

    Add 404 NotFound.vue

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import NotFound from "../views/NotFound.vue";
 
 Vue.use(VueRouter);
 
@@ -24,7 +25,9 @@ const routes = [
     name: "Test",
     component: () => 
       import(/* webpackChunkName: "test" */ "../views/Test.vue")
-  }
+  },
+  { path: '/404', component: NotFound },  
+  { path: '*', redirect: '/404' }
 ];
 
 const router = new VueRouter({
